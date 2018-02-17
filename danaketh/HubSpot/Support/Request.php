31f303fba72a4df88dab8320714aa1a2df7d7d9e
danaketh <daniel@tlach.cz>
2018-02-17 14:47:13 +0100

    Implemented GET method and added stub for PATCH

--- danaketh/HubSpot/Support/Request.php
+++ danaketh/HubSpot/Support/Request.php
@@ -2,6 +2,10 @@
 
 namespace danaketh\HubSpot\Support;
 
+use danaketh\HubSpot\Exception\RequestException;
+
+
+
 /**
  * cURL wrapper
  *
@@ -10,8 +14,36 @@ namespace danaketh\HubSpot\Support;
  */
 class Request
 {
-    public static function get($url)
+    /**
+     * @param string $url
+     *
+     * @return array
+     * @throws RequestException
+     */
+    public static function get($url): array
     {
+        $instance = curl_init($url);
+        curl_setopt_array($instance, [
+            CURLOPT_RETURNTRANSFER => true,
+        ]);
+        $request = curl_exec($instance);
+
+        if (!$request) {
+            throw new RequestException(curl_error($instance));
+        }
+
+        $info = curl_getinfo($instance);
+        curl_close($instance);
+
+        $response = [
+            'code'         => $info['http_code'],
+            'body'         => $info['content_type'] === 'application/json'
+                ? json_decode($request, JSON_OBJECT_AS_ARRAY)
+                : $request,
+            'content-type' => $info['content_type'],
+        ];
+
+        return $response;
     }
 
 
@@ -32,6 +64,13 @@ class Request
 
 
     public static function delete($url)
+    {
+    }
+
+
+
+
+    public static function patch($url)
     {
     }
 }
